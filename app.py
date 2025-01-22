652fcf9c6021d3b4062ef6661b28d465da12ad92
Kaoru <kaswister0116@gmail.com>
2025-01-22 13:31:49 +0900

    DBコンテナ追加

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@mysql-db/test_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    def select_user(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
 
 @app.route('/')
 def hello_world():
@@ -10,5 +27,11 @@ def hello_world():
 def test():
     return "test"
 
+@app.route('/users')
+def get_users():
+    users = User.query.all()
+    return jsonify([user.select_user() for user in users])
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
